8e303c860b0315ed46ef4f74247bf52d2dfd0401
Eugene Akentev <ak3ntev@gmail.com>
2018-04-07 15:31:21 +0500

    wip

--- game/js/index.js
+++ game/js/index.js
@@ -1,4 +1,7 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'test', null, true, false);
+var game = new Phaser.Game(1024, 800, Phaser.AUTO, 'test', null, true, false);
+
+var health = 0;
+var score = 0;
 
 var BasicGame = function (game) { };
 
@@ -10,17 +13,14 @@ BasicGame.Boot.prototype =
 {
     preload: function () {
         game.load.image('tile', '../img/tile.png');
+        game.load.image('tower', '../img/tower.png');
 
         game.time.advancedTiming = true;
-
-        // Add and enable the plug-in.
         game.plugins.add(new Phaser.Plugin.Isometric(game));
 
         // This is used to set a game canvas-based offset for the 0, 0, 0 isometric coordinate - by default
         // this point would be at screen coordinates 0, 0 (top left) which is usually undesirable.
-        game.iso.anchor.setTo(0.5, 0.5);
-
-
+        game.iso.anchor.setTo(0.5, 0.2);
     },
     create: function () {
 
@@ -58,17 +58,22 @@ BasicGame.Boot.prototype =
     },
     render: function () {
         game.debug.text(game.time.fps || '--', 2, 14, "#a7aebe");
-    },
+        game.debug.text(health + " / 100" || '--', 2, 36, "#a7aebe");
+        game.debug.text("$ " + score || '--', 2, 54, "#a7aebe");
+      },
     spawnTiles: function () {
         var tile;
-        for (var xx = 0; xx < 256; xx += 38) {
-            for (var yy = 0; yy < 256; yy += 38) {
+        for (var xx = 0; xx < 512; xx += 19) {
+            for (var yy = 0; yy < 512; yy += 19) {
                 // Create a tile using the new game.add.isoSprite factory method at the specified position.
                 // The last parameter is the group you want to add it to (just like game.add.sprite)
                 tile = game.add.isoSprite(xx, yy, 0, 'tile', 0, isoGroup);
                 tile.anchor.set(0.5, 0);
             }
         }
+
+        tile = game.add.isoSprite(-72, -72, 0, 'tower', 0, isoGroup);
+        tile.anchor.set(0.5, 0.5);
     }
 };
 
